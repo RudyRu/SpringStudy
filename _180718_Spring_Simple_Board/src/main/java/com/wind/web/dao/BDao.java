0e64bf796da40a5b14b2c2fb7bba5fd09e70873b
RudyRu <pinkflow777@gmail.com>
2018-07-19 14:19:48 +0900

    SpringLecture 2018-07-19

---
+++ _180718_Spring_Simple_Board/src/main/java/com/wind/web/dao/BDao.java
@@ -0,0 +1,122 @@
+package com.wind.web.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+import com.wind.web.dto.BDto;
+
+public class BDao {
+	
+	DataSource dataSource;
+	
+	public BDao() {		
+		try {
+			Context context = new InitialContext();
+			dataSource = (DataSource) context.lookup("java:comp/env/jdbc/Oracle11g");
+		} catch (NamingException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public ArrayList<BDto> list(){
+		ArrayList<BDto> dtos = new ArrayList<BDto>();
+		BDto dto;
+		
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+		ResultSet resultSet = null;
+		
+		try {
+			connection = dataSource.getConnection();
+			String query = "select bId, bName, bTitle, bContent, "
+					+ "bDate, bHit, bGroup, bStep, bIndent "
+					+ "from mvc_board order by bGroup desc, bStep asc";
+			preparedStatement = connection.prepareStatement(query);
+			resultSet = preparedStatement.executeQuery();
+			
+			while(resultSet.next()) {
+				int bId = resultSet.getInt("bId");
+				String bName = resultSet.getString("bName");
+				String bTitle = resultSet.getString("bTitle");
+				String bContent = resultSet.getString("bContent");
+				Timestamp bDate = resultSet.getTimestamp("bDate");
+				int bHit = resultSet.getInt("bHit");
+				int bGroup = resultSet.getInt("bGroup");
+				int bStep = resultSet.getInt("bStep");
+				int bIndent = resultSet.getInt("bIndent");
+				
+				dto = new BDto(bId, bName, bTitle, bContent, bDate, bHit, bGroup, bStep, bIndent);
+				dtos.add(dto);
+			}
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				if(resultSet != null) resultSet.close();
+				if(preparedStatement != null) resultSet.close();
+				if(connection != null) resultSet.close();
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return dtos;
+	}
+	
+	public BDto contentView(String strID) {
+			
+		//upHit(strID);
+		BDto dto = null;
+		
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+		ResultSet resultSet = null;
+		
+		try {
+			connection = dataSource.getConnection();
+			String query = "select * from mvc_board where bId = ?";
+			
+			preparedStatement = connection.prepareStatement(query);
+			preparedStatement.setInt(1, Integer.parseInt(strID));
+			resultSet = preparedStatement.executeQuery();
+			
+			if(resultSet.next()) {
+				int bId = resultSet.getInt("bId");
+				String bName = resultSet.getString("bName");
+				String bTitle = resultSet.getString("bTitle");
+				String bContent = resultSet.getString("bContent");
+				Timestamp bDate = resultSet.getTimestamp("bDate");
+				int bHit = resultSet.getInt("bHit");
+				int bGroup = resultSet.getInt("bGroup");
+				int bStep = resultSet.getInt("bStep");
+				int bIndent = resultSet.getInt("bIndent");
+				
+				dto = new BDto(bId, bName, bTitle, bContent, bDate, bHit, bGroup, bStep,
+						bIndent);
+			}
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				if(resultSet != null) resultSet.close();
+				if(preparedStatement != null) resultSet.close();
+				if(connection != null) resultSet.close();
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return dto;		
+	}
+	
+}
